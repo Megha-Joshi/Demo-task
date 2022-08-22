96774bed4a23261a30df25760c1034827d0528b8
Megha-Joshi <megha24joshi@gmail.com>
2022-08-23 01:10:30 +0530

    initial-setup

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCoJ5ilw54lf49nYPU1rQZDM46oZcHeKzE",
+    authDomain: "fir-task-82644.firebaseapp.com",
+    projectId: "fir-task-82644",
+    storageBucket: "fir-task-82644.appspot.com",
+    messagingSenderId: "1031004701594",
+    appId: "1:1031004701594:web:11198d9441236c83a4f023",
+    measurementId: "G-5CFFKEECXF"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export const db = getFirestore(app);
